3a468b5e0b530d5799681cdfaaa54f630e1464a0
jitesh456 <jitesh0dubey@gmail.com>
2020-05-11 23:32:20 +0530

    ADDED:Feature for Showing Book Info

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -4,6 +4,27 @@ import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
+import { withStyles} from "@material-ui/core/styles";
+import Tooltip from "@material-ui/core/Tooltip";
+import Typography from "@material-ui/core/Typography";
+import MuiCardContent from "@material-ui/core/CardContent";
+
+const HtmlTooltip = withStyles(theme => ({
+    tooltip: {
+      backgroundColor: "#f5f5f9",
+      color: "rgba(0, 0, 0, 0.97)",
+      maxWidth: 220,
+      fontSize: theme.typography.pxToRem(12),
+      color:"grey",
+      border: "1px solid #dadde9"
+    }
+  }))(Tooltip);
+  const NestedCardContent = withStyles(theme => ({
+    root: {
+      padding: 0
+    
+    }
+  }))(MuiCardContent);
 
 export default class BookCard extends Component {
 
@@ -13,38 +34,78 @@ export default class BookCard extends Component {
             data: []
         }
     }
-
-
-
     render() {
         return (
-            <div className="cardofbook" style={{ marginTop: '25px' }}>
-
-                <Card className="baseCard" >
-                    <CardContent className="cardData">
-                        <div className="firstPart">
-
-                            <img src={this.props.bookDetails.bookcover} className="image" />
+            <div className="cardofbook" style={{ marginTop: '20px' }}>
+                    <Card style={{ height:"300px" ,width:"200px",padding:"0px",backgroundColor:"Snow"}}>
+                    <NestedCardContent>
+                    <div style={{ height:"20px",display:"flex" ,backgroundColor:"lightGrey",justifyContent:"flex-end",margin:"0px",paddingRight:"8px"}}>
+                        <HtmlTooltip 
+                             title={
+                                    <React.Fragment>
+                                        <Typography color="black"><b>Book Details</b></Typography>
+                                        <em>{this.props.bookDetails.Discription}</em>         
+                                    </React.Fragment>
+                                    }
+                                >
+                            <span style={{ fontWeight:"bold" }}>...</span>
+                        </HtmlTooltip>
                         </div>
-                        <div ClassName="secondPart">
-                            <div className="book_Detail">
-                                <div style={{ fontSize: 17 },{fontWeight: 600}}>{this.props.bookDetails.name}</div>
-                                <div style={{ fontSize: 12 }}> By,{this.props.bookDetails.auther}</div>
-                                <div style={{ fontSize: 13 }} classNme="price">Price:{this.props.bookDetails.price}</div>
+                        <div style={{ width:"100%"}}>
+                            <div id= "container" style={{ backgroundColor:"lightGrey",height:"140px",display:"flex", justifyContent:"center"}}>
+                                <img  src={this.props.bookDetails.src} className="image" alt=""/>
                             </div>
-                            <CardActions>
-                                <div className="buttonArea">
-                                    <Button type="submit" variant="contained" size="45%" style={{ backgroundColor: 'Maroon', color: "white" }} >
-                                        Add to Cart
-                                 </Button>
-                                </div>
-                            </CardActions>
-
+                            <div className="book_Detail" style={{ paddingLeft:"10px",width:"100%",paddingTop:"5px"}}>
+                                <span style={{ fontSize:"14px"}}>Harry Potter The Philishoper Stone</span>
+                                <span style={{ fontSize:"12px"}}>{this.props.bookDetails.Auther}</span>
+                                <span style={{ fontSize:"12px", fontWeight:"bold"}}>Rs:{this.props.bookDetails.price}</span>
+                            </div>
                         </div>
-
+                        <div style={{ width:"100%", display:"flex",justifyContent:"center"}}>
+                        <CardActions>
+                            <Button type="submit" variant="contained" size="50%" style={{ backgroundColor: 'Maroon', color: "white" }} >
+                                Add to Cart
+                            </Button>
+                        </CardActions>
+                            </div>
+                    </NestedCardContent>
+                    </Card>
+{/* 
+                 <Card  style={{ border:"2px solid red",padding:"0px"}} >
+                    <NestedCardContent>
+                    <CardContent className="cardData" style={{ border:"2px solid red"}}>
+                        <div style={{ width:"100%",height:"20px",display:"flex" ,justifyContent:"flex-end",border:"2px solid black",margin:"0px"}}>
+                        <HtmlTooltip
+                             title={
+                                    <React.Fragment>
+                                        <Typography color="black"><b>Book Details</b></Typography>
+                                        <em>{this.props.bookDetails.Discription}</em>         
+                                    </React.Fragment>
+                                    }
+                                >
+                            <Button style={{ width:"2px", backgroundColor:"lightgrey" }}>...</Button>
+                        </HtmlTooltip>
+                        </div>
+                        <div style={{ width:"100%",border:"2px solid blue"}}>
+                            <div id= "container" style={{ marginTop:"20px",height:"100px",border:"2px solid green"}}>
+                                <img  src={this.props.bookDetails.src} className="image" alt=""/>
+                            </div>
+                            <div className="book_Detail" style={{ border:"2px solid purple"}}>
+                                <span>Book Name:Harry Potter</span>
+                                <span>Auther:{this.props.bookDetails.Auther}</span>
+                                <span>Price:{this.props.bookDetails.price}</span>
+                            </div>
+                        </div>
+                      
+                        <CardActions>
+                            <Button type="submit" variant="contained" size="50%" style={{ backgroundColor: 'Maroon', color: "white" }} >
+                                Add to Cart
+                            </Button>
+                        </CardActions>
                     </CardContent>
-                </Card>
-            </div>
+                    </NestedCardContent>
+                </Card> */}
+            </div> 
         )
     }
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -5,7 +5,15 @@ import '../css/Pagination.css';
 import BookCard from "./BookCard";
 import booklogo from '../booklogo.png';
 import ReactPaginate from 'react-paginate';
-import Service from '../service/Service'
+import Service from '../service/Service';
+import image0 from '../assets/images/image0.jpg';
+import image1 from '../assets/images/image1.jpg';
+import image2 from '../assets/images/image2.jpg';
+import image3 from '../assets/images/image3.jpg';
+import image4 from '../assets/images/image4.jpg';
+import image5 from '../assets/images/image5.jpg';
+import image6 from '../assets/images/image6.jpg';
+import image7 from '../assets/images/image7.jpg';
 
 
 class Main extends Component {
@@ -15,23 +23,32 @@ class Main extends Component {
 			offset: 0,
 			perPage: 8,
 			currentPage: 0,
-            booklist:[]
-        }
+            booklist:[{ id: 0, src: image0, Auther: 'foo', price: 150 ,
+            Discription:"It is known from archaeological evidence that a highly sophisticated urbanized culture—the Indus civilization—dominated the northwestern part of the subcontinent from about 2600 to 2000 BCE."},
+            { id: 1, src: image1, Auther: 'foo', price: 150 ,Discription:"It is known from archaeological evidence that a highly sophisticated urbanized culture—the Indus civilization—dominated the northwestern part of the subcontinent from about 2600 to 2000 BCE."},
+            { id: 2, src: image2, Auther: 'foo', price: 150 ,Discription:"It is known from archaeological evidence that a highly sophisticated urbanized culture—the Indus civilization—dominated the northwestern part of the subcontinent from about 2600 to 2000 BCE."},
+            { id: 3, src: image3, Auther: 'foo', price: 150 ,Discription:"It is known from archaeological evidence that a highly sophisticated urbanized culture—the Indus civilization—dominated the northwestern part of the subcontinent from about 2600 to 2000 BCE."},
+            { id: 4, src: image4, Auther: 'foo', price: 150 ,Discription:"It is known from archaeological evidence that a highly sophisticated urbanized culture—the Indus civilization—dominated the northwestern part of the subcontinent from about 2600 to 2000 BCE."},
+            { id: 5, src: image5, Auther: 'foo', price: 150 ,Discription:"It is known from archaeological evidence that a highly sophisticated urbanized culture—the Indus civilization—dominated the northwestern part of the subcontinent from about 2600 to 2000 BCE."},
+            { id: 6, src: image6, Auther: 'foo', price: 150, Discription:"It is known from archaeological evidence that a highly sophisticated urbanized culture—the Indus civilization—dominated the northwestern part of the subcontinent from about 2600 to 2000 BCE."}
+
+        ]}
+
         this.handlePageClick=this.handlePageClick.bind(this);
     } 
 
-    componentDidMount(){   
-        Service.getBookData().then((response)=>{
-            console.log(response);
-            this.setState({
-                booklist:response.data.body
-            })  
-            this.receivedData();
-            console.log(this.state.booklist)
-        }).catch((error)=>{
-        console.log(error)
-        }) 
-    }
+    // componentDidMount(){   
+    //     Service.getBookData().then((response)=>{
+    //         console.log(response);
+    //         this.setState({
+    //         //    booklist:response.data.body
+    //         })  
+    //         this.receivedData();
+    //         console.log(this.state.booklist)
+    //     }).catch((error)=>{
+    //     console.log(error)
+    //     }) 
+    // }
 
     receivedData() {
 			  const data = this.state.booklist;
@@ -62,9 +79,9 @@ class Main extends Component {
 			  });
 	}
 
-    //  componentDidMount(){
-    //      this.receivedData(); 
-    //  }
+     componentDidMount(){
+         this.receivedData(); 
+     }
 
     render() {
 
