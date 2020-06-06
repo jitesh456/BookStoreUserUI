23bd073ae3d199ff58a872e5729f74179134a412
supriyaphalle <supriyaphalle@gmail.com>
2020-06-06 07:57:27 +0530

    REFACTORED: Opened Dialog Box On ADD To Cart If User IS Not LoggedIn

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import '../css/BookCard.css';
+import DialogBox from "./DialogBox";
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import Button from '@material-ui/core/Button';
@@ -11,12 +12,12 @@ import history from './history';
 
 const ImageToolTip = withStyles((theme) => ({
     tooltip: {
-      color: 'FCF2F2',
-      maxWidth: 265,
-      fontSize: theme.typography.pxToRem(12),
+        color: 'FCF2F2',
+        maxWidth: 265,
+        fontSize: theme.typography.pxToRem(12),
     },
-  }))(Tooltip);
-    
+}))(Tooltip);
+
 
 const NestedCardContent = withStyles(theme => ({
     root: {
@@ -26,16 +27,16 @@ const NestedCardContent = withStyles(theme => ({
 }))(MuiCardContent);
 
 export default class BookCard extends Component {
-    
+
     constructor(props) {
         super(props);
         this.state = {
             data: [],
-            
+            isDialogOpen: false,
         }
-        
+
     }
-    
+
     displayImage() {
         if (this.props.bookDetails.quantity === 0) {
             return (
@@ -51,74 +52,83 @@ export default class BookCard extends Component {
         }
     }
 
+    openDialog = () => {
+        this.setState({ isDialogOpen: true })
+        console.log(this.state.isDialogOpen);
+    }
+
+    handleClose = () => this.setState({ isDialogOpen: false })
+
+
     displayButton() {
-        if(localStorage.getItem("token")==null)
-        {
-           return (
-                <Button  type="submit" variant="contained" onClick={()=>{
-                    history.push('/')
-                }} size="50%" style={{ width:"90%", backgroundColor:'maroon', color: "white" }} >ADD TO CART</Button>
-            );  
+        if (localStorage.getItem("token") == null) {
+            return (
+                <Button type="submit" variant="contained"
+                    onClick={this.openDialog}
+                    size="50%" style={{ width: "90%", backgroundColor: 'maroon', color: "white" }} >ADD TO CART</Button>
+            );
         }
         if (this.props.bookDetails.quantity === 0) {
             return (
-                <Button  type="submit" variant="contained"  disabled={ !this.props.bookDetails.quantity} size="50%" style={{ width:"90%", backgroundColor:'silver', color: "black" }} >ADD TO CART</Button>
+                <Button type="submit" variant="contained" disabled={!this.props.bookDetails.quantity} size="50%" style={{ width: "90%", backgroundColor: 'silver', color: "black" }} >ADD TO CART</Button>
             );
         }
-        const bookName=JSON.parse(localStorage.getItem("bookName"));
-        if( bookName!==null && bookName.includes(this.props.bookDetails.name)){
-            return(
-                    <Button  type="submit" name="addButton" variant="contained" onClick={()=>{
-                    history.push('/cart')
-                }} disabled={ !this.props.bookDetails.quantity} size="50%" style={{ width:"90%", backgroundColor:"#A52A20", color: "whitesmoke"}} >GO TO CART</Button>
-            );
-        }
-         else {
+        const bookName = JSON.parse(localStorage.getItem("bookName"));
+        if (bookName !== null && bookName.includes(this.props.bookDetails.name)) {
             return (
-                <Button  
-                    name= "button" type="submit" variant="contained" onClick={(event)=>{
-                    this.props.addFunction(this.props.bookDetails)
-                       
-                }} disabled={ !this.props.bookDetails.quantity} size="50%" style={{ width:"90%", backgroundColor:'maroon', color: "white" }} >ADD TO CART</Button>
+                <Button type="submit" name="addButton" variant="contained" onClick={() => {
+                    history.push('/cart')
+                }} disabled={!this.props.bookDetails.quantity} size="50%" style={{ width: "90%", backgroundColor: "#A52A20", color: "whitesmoke" }} >GO TO CART</Button>
+            );
+        }
+        else {
+            return (
+                <Button
+                    name="button" type="submit" variant="contained" onClick={(event) => {
+                        this.props.addFunction(this.props.bookDetails)
+
+                    }} disabled={!this.props.bookDetails.quantity} size="50%" style={{ width: "90%", backgroundColor: 'maroon', color: "white" }} >ADD TO CART</Button>
             );
         }
 
     }
-   
+
     render() {
-        
+
         return (
             <div className="cardofbook" name="cardData"  >
                 <Card className="card"  >
                     <NestedCardContent>
-                        <div >                            
-                                <ImageToolTip
-                                        title={
-                                            <React.Fragment>
-                                                <Typography ><b>Book Details</b></Typography>
-                                                <p>{this.props.bookDetails.bookdetails}</p>
-                                            </React.Fragment>
-                                        }
-                                        placement="right-start"
-                                        arrow
-                                    >
-                                    <div id="bookimage">
-                                        { this.displayImage()}
-                                    </div>                         
-                                    </ImageToolTip>              
+                        <div >
+                            <ImageToolTip
+                                title={
+                                    <React.Fragment>
+                                        <Typography ><b>Book Details</b></Typography>
+                                        <p>{this.props.bookDetails.bookdetails}</p>
+                                    </React.Fragment>
+                                }
+                                placement="right-start"
+                                arrow
+                            >
+                                <div id="bookimage">
+                                    {this.displayImage()}
+                                </div>
+                            </ImageToolTip>
                             <div className="book_detail">
                                 <span className="book_name">{this.props.bookDetails.name}</span>
                             </div>
-                            <div className="space-between"></div>    
+                            <div className="space-between"></div>
                             <div className="book_detail">
                                 <span className="book_author">{this.props.bookDetails.authorname}</span>
                                 <span className="book_price">Rs:{this.props.bookDetails.price}</span>
                             </div>
                         </div>
-                        <div style={{ marginBottom:"40px" }}>
+                        <div style={{ marginBottom: "40px" }}>
                             <CardActions className="card_action">
-                               {this.displayButton()}
+                                {this.displayButton()}
                             </CardActions>
+                            <DialogBox DialogOpen={this.state.isDialogOpen}
+                                DialogClose={this.handleClose} />
                         </div>
                     </NestedCardContent>
                 </Card>
